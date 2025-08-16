ab71882c8af4106a95739f0f09c70eb74471d578
PlatinumPlat <twinsophiapu@gmail.com>
2025-08-15 22:28:16 -0400

    grokapi, explore page recipes

---
+++ Scripts/whisk.js
@@ -0,0 +1,9 @@
+/*
+import { groq } from '@ai-sdk/groq';
+import { generateText } from 'ai';
+
+const { text } = await generateText({
+  model: groq('llama-3.3-70b-versatile'),
+  prompt: 'Write a vegetarian lasagna recipe for 4 people.',
+});
+*/
