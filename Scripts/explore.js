170d809fe8c6f711d5e92a49de39074bed1eab27
PlatinumPlat <twinsophiapu@gmail.com>
2025-08-16 12:27:48 -0400

    publish page, explore changes, publish and explore connection

--- Scripts/explore.js
+++ Scripts/explore.js
@@ -1,3 +1,31 @@
+const container = document.querySelector(".recipecontainer");
+
+function loadRecipes() {
+    const recipes = JSON.parse(localStorage.getItem("recipes")) || []
+    document.querySelectorAll(".userRecipe").forEach(el => el.remove());
+
+    recipes.forEach((r, index) => {
+        const card =  document.createElement("div");
+        card.className = `userRecipe ${r.type}Recipes`;
+
+        card.innerHTML = `
+            <h2 class="exploretitle">${r.title}</h2>
+            <p>${r.desc}</p>
+            <p><em>Type: ${r.type}</em></p>
+            <button onclick="deleteRecipe(${index})">Delete</button>
+        `;
+
+        container.appendChild(card);
+    });
+}
+
+function deleteRecipe(index) {
+    const recipes = JSON.parse(localStorage.getItem("recipes")) || [];
+    recipes.splice(index, 1);
+    localStorage.setItem("recipes", JSON.stringify(recipes));
+    loadRecipes();
+}
+
 function cookingRecipes() {
     document.querySelectorAll(".bakingRecipes").forEach(el => el.style.display = "none");
     document.querySelectorAll(".cookingRecipes").forEach(el => el.style.display = "block");
@@ -23,3 +51,7 @@ document.getElementById("baking").addEventListener("click", function () {
     document.getElementById("cooking").classList.add("inactive");
     bakingRecipes();
 });
+
+
+loadRecipes();
+cookingRecipes();
---
+++ Scripts/publish.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('recipeForm');
+const container = document.getElementById('recipeContainer');
+
+function loadRecipes() {
+    container.innerHTML = "";
+    const recipes = JSON.parse(localStorage.getItem("recipes")) || [];
+    recipes.forEach((r, index) => {
+        const card = document.createElement("div");
+        card.className = `recipe ${r.type}Recipes`;
+        card.innerHTML = `
+            <h2 class="exploretitle">${r.title}</h2>
+            <p>${r.desc}</p>
+            <p><em>Type: ${r.type}</em></p>
+            <button onclick="deleteRecipe(${index})">Delete</button>
+            `;
+        container.appendChild(card);
+    });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const title = document.getElementById("recipeTitle").value;
+    const desc = document.getElementById("recipeDescription").value;
+    const type = document.getElementById("recipeType").value;
+    const recipes = JSON.parse(localStorage.getItem("recipes")) || [];
+
+    recipes.push({ title, desc, type })
+    localStorage.setItem("recipes", JSON.stringify(recipes));
+
+    form.reset();
+    loadRecipes();
+});
+
+function deleteRecipe(index) {
+    const recipes = JSON.parse(localStorage.getItem("recipes")) || [];
+    recipes.splice(index, 1);
+    localStorage.setItem("recipes", JSON.stringify(recipes));
+    loadRecipes();
+}
+
+loadRecipes();
